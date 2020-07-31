80b7fb55247990132f9f25b70819ee5f868f54b7
Paulo Cardoso <paulo@agily.com.br>
2020-07-31 14:27:45 -0300

    starting project creating migration for Post and moving User to Models folder

---
+++ database/migrations/2020_07_31_172058_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id')->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+            $table->string('title')->unique();
+            $table->string('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
